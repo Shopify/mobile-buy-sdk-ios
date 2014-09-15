82bdf1fd6436257440b3451dbf9bf642946e67d5
Joshua Tessier <josh@shopify.com>
2014-09-15 17:04:31 -0400

    Moves NSDecimalNumber additions to a proper category

--- Storefront/Storefront/Models/MERProductVariant.m
+++ Storefront/Storefront/Models/MERProductVariant.m
@@ -8,20 +8,8 @@
 
 #import "MERProductVariant.h"
 
-@implementation NSDecimalNumber (Additions)
-+ (NSDecimalNumber*)decimalNumberOrZeroWithString:(NSString*)string
-{
-	NSDecimalNumber *decimalNumber = nil;
-	if (string) {
-		decimalNumber = [NSDecimalNumber decimalNumberWithString:string];
-	}
-	
-	if (decimalNumber == nil || decimalNumber == [NSDecimalNumber notANumber]) {
-		decimalNumber = [NSDecimalNumber zero];
-	}
-	return decimalNumber;
-}
-@end
+//Additions
+#import "NSDecimalNumber+MERAdditions.h"
 
 @implementation MERProductVariant
 
@@ -34,45 +22,15 @@
 		self.option2 = dictionary[@"option2"];
 		self.option3 = dictionary[@"option3"];
 		
-		self.price = [MERProductVariant decimalNumberFromJSON:dictionary[@"price"]];
-		self.compareAtPrice = [MERProductVariant decimalNumberFromJSON:dictionary[@"compare_at_price"]];
-		self.grams = [MERProductVariant decimalNumberFromJSON:dictionary[@"grams"]];
+		self.price = [NSDecimalNumber decimalNumberFromJSON:dictionary[@"price"]];
+		self.compareAtPrice = [NSDecimalNumber decimalNumberFromJSON:dictionary[@"compare_at_price"]];
+		self.grams = [NSDecimalNumber decimalNumberFromJSON:dictionary[@"grams"]];
 		
 		self.requiresShipping = dictionary[@"requires_shipping"];
 		self.taxable = dictionary[@"taxable"];
 		self.position = dictionary[@"position"];
 	}
 	return self;
-}
-
-+ (NSDecimalNumber*)decimalNumberFromJSON:(id)valueFromJSON
-{
-	static NSDecimalNumberHandler *numberHandler;
-	static dispatch_once_t onceToken;
-	dispatch_once(&onceToken, ^{
-		numberHandler = [[NSDecimalNumberHandler alloc] initWithRoundingMode:NSRoundBankers scale:12 raiseOnExactness:NO raiseOnOverflow:NO raiseOnUnderflow:NO raiseOnDivideByZero:NO];
-	});
-	
-	NSDecimalNumber *decimalNumber = nil;
-	if (valueFromJSON == nil || [valueFromJSON isKindOfClass:[NSNull class]]) {
-		decimalNumber = nil;
-	}
-	else if ([valueFromJSON isKindOfClass:[NSString class]]) {
-		decimalNumber = [NSDecimalNumber decimalNumberOrZeroWithString:valueFromJSON];
-	}
-	else if ([valueFromJSON isKindOfClass:[NSDecimalNumber class]]) {
-		decimalNumber = valueFromJSON;
-	}
-	else if ([valueFromJSON isKindOfClass:[NSNumber class]]) {
-		NSDecimal decimal = [(NSNumber*)valueFromJSON decimalValue];
-		decimalNumber = [NSDecimalNumber decimalNumberWithDecimal:decimal];
-	}
-	else {
-		decimalNumber = nil;
-		NSLog(@"Could not create decimal value: %@", valueFromJSON);
-	}
-	
-	return [decimalNumber decimalNumberByRoundingAccordingToBehavior:numberHandler];
 }
 
 @end
