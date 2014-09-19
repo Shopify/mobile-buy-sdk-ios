c4328e8891d9aa56eff6b86ac59e87aa1be070cc
Joshua Tessier <josh@shopify.com>
2014-09-19 12:58:46 -0400

    Updates project file to support CocoaPods in Tests

---
+++ CheckoutAnywhere/CheckoutAnywhereTests/CHKAnywhereIntegrationTest.m
@@ -0,0 +1,28 @@
+//
+//  CHKAnywhereIntegrationTest.m
+//  CheckoutAnywhere
+//
+//  Created by Joshua Tessier on 2014-09-19.
+//  Copyright (c) 2014 Shopify Inc. All rights reserved.
+//
+
+@import UIKit;
+@import XCTest;
+
+#import "CHKDataProvider.h"
+#import "MERDataProvider.h"
+
+@interface CHKAnywhereIntegrationTest : XCTestCase
+
+@end
+
+@implementation CHKAnywhereIntegrationTest {
+	
+}
+
+- (void)testCheckoutAnywhereFLow
+{
+	
+}
+
+@end
