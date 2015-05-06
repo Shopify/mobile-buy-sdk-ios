6cf17bffa6a444bad329ffab411e83d62ac48ca8
Rune Madsen <runemadsen@mac.com>
2015-05-06 08:40:47 -0400

    Updated documentation for CHKStoreViewController

--- CheckoutAnywhere/CheckoutAnywhere/View Controllers/CHKStoreViewController.h
+++ CheckoutAnywhere/CheckoutAnywhere/View Controllers/CHKStoreViewController.h
@@ -25,31 +25,35 @@ typedef NS_ENUM(NSUInteger, CHKCheckoutError) {
 
 typedef void (^CHKCheckoutTypeBlock)(CHKCheckoutType type);
 
-
 @class CHKStoreViewController;
 
 @protocol CHKStoreViewControllerDelegate <CHKViewControllerDelegate>
 
 /**
- * Tells the delegate that the user has proceeded to checkout.  Use this opportunity to present an interface the the
- * user to choose between checking out with ApplePay or standard webcheckout. Before presenting an option for ApplePay, check if the device is setup to do so by calling `[PKPaymentAuthorizationViewController canMakePayments]`
+ *  Tells the delegate that the user has proceeded to checkout. Use this opportunity to present an interface to a
+ *  user to choose between checking out with Apple Pay or standard webcheckout.
+ *  Note: Before presenting an option for Apple Pay, check if the device is setup to do so by calling `[PKPaymentAuthorizationViewController canMakePayments]`
+ *
+ *  @param controller        The controller with an embedded WKWebView displaying the Shopify store
+ *  @param completionHandler (^CHKCheckoutTypeBlock)(CHKCheckoutType type);
  */
 - (void)controller:(CHKStoreViewController *)controller shouldProceedWithCheckoutType:(CHKCheckoutTypeBlock)completionHandler;
 
 @end
 
 /**
- * This example controller shows you how to build a controller that embeds a WKWebView (iOS 8+) with your store in it. This means that
- * you can show off your store and its sleek responsive design without having to build a full native app to showcase it.
+ *  This example controller shows you how to build a controller that embeds a WKWebView (iOS 8+) with your store in it. 
+ *  This means that you can show off your store and its sleek responsive design without having to build a full native app to showcase it.
  *
- * There are a few approaches that you can take, this app shows you a couple:
+ *  There are a few approaches that you can take, this app shows you a couple:
  *
- * 1. Only present ApplePay to /checkout. When the user attempts to press 'Checkout' you can present the user the ApplePay button, or allow them to continue on as normal.
- *    This approach does NOT require any changes to the actual storefront.
+ *  1. Only present Apple Pay in your /checkout. When the user taps 'Checkout' you can present the user an 
+ *     Apple Pay button, or * allow them to continue on as normal. This approach does NOT require any changes 
+ *     to the web storefront.
  *
- * 2. Add ApplePay 'Pay' buttons on individual product pages and on checkout.
- *    This approach REQUIRES a few small changes to the storefront to work. (See .m file)
- *    You will need **some** knowledge of liquid and Shopify's theming system to get this working.
+ *  2. Add 'Buy With  Pay' buttons on individual product pages and/or a ' Pay' button in your /checkout.
+ *     This approach REQUIRES a few small changes to the web storefront to work. Refer to the app.js file 
+ *     located within the SDK. General knowledge of Liquid and Shopify's theming is required system to get this working.
  */
 @interface CHKStoreViewController : CHKViewController
 
