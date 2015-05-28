04cfdd4e030b2182cf779910e1753503af6bdc3d
David Muzi <david.muzi@shopify.com>
2015-05-28 10:39:04 -0400

    Initial work for web URL

---
+++ Mobile Buy SDK/Mobile Buy SDK/Utils/BUYClient+WebCheckout.h
@@ -0,0 +1,13 @@
+//
+//  CHKClient+WebCheckout.h
+//  Mobile Buy SDK
+//
+//  Created by David Muzi on 2015-05-28.
+//  Copyright (c) 2015 Shopify Inc. All rights reserved.
+//
+
+
+
+@interface CHKClient (WebCheckout)
+
+@end
---
+++ Mobile Buy SDK/Mobile Buy SDK/Utils/BUYClient+WebCheckout.m
@@ -0,0 +1,13 @@
+//
+//  CHKClient+WebCheckout.m
+//  Mobile Buy SDK
+//
+//  Created by David Muzi on 2015-05-28.
+//  Copyright (c) 2015 Shopify Inc. All rights reserved.
+//
+
+#import "CHKClient+WebCheckout.h"
+
+@implementation CHKClient (WebCheckout)
+
+@end
