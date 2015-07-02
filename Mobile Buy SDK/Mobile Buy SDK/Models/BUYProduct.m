68daa5c5fc7dd386a9d40bfd50f93f29e8b13e72
David Muzi <david.muzi@shopify.com>
2015-07-02 16:23:49 -0400

    adds availability boolean to products

--- Mobile Buy SDK/Mobile Buy SDK/Models/BUYProduct.m
+++ Mobile Buy SDK/Mobile Buy SDK/Models/BUYProduct.m
@@ -26,6 +26,7 @@
 	_images = [BUYImage convertJSONArray:dictionary[@"images"]];
 	_options = [BUYOption convertJSONArray:dictionary[@"options"]];
 	_htmlDescription = dictionary[@"body_html"];
+	_available = [dictionary[@"available"] boolValue];
 }
 
 @end
