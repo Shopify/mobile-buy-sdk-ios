d4f76322108adabda5fbec8635aebd2c407e628b
Rune Madsen <runemadsen@mac.com>
2015-07-07 14:57:40 -0400

    adding footer view

---
+++ Mobile Buy SDK/Mobile Buy SDK/Views/BUYProductViewFooter.h
@@ -0,0 +1,13 @@
+//
+//  BUYProductViewFooter.h
+//  Mobile Buy SDK
+//
+//  Created by Rune Madsen on 2015-07-07.
+//  Copyright (c) 2015 Shopify Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BUYProductViewFooter : UIView
+
+@end
---
+++ Mobile Buy SDK/Mobile Buy SDK/Views/BUYProductViewFooter.m
@@ -0,0 +1,21 @@
+//
+//  BUYProductViewFooter.m
+//  Mobile Buy SDK
+//
+//  Created by Rune Madsen on 2015-07-07.
+//  Copyright (c) 2015 Shopify Inc. All rights reserved.
+//
+
+#import "BUYProductViewFooter.h"
+
+@implementation BUYProductViewFooter
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
