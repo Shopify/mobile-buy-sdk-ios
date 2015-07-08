ea46fc661ea5e7f09dc1db4f35eed8dd1a439b69
Rune Madsen <runemadsen@mac.com>
2015-07-08 11:11:32 -0400

    image view

--- Mobile Buy SDK/Mobile Buy SDK/Views/BUYProductViewHeader.m
+++ Mobile Buy SDK/Mobile Buy SDK/Views/BUYProductViewHeader.m
@@ -8,6 +8,14 @@
 
 #import "BUYProductViewHeader.h"
 
+@interface BUYProductViewHeader ()
+
+@property (nonatomic, strong) UIImageView *backgroundImageView;
+@property (nonatomic, strong) NSArray *backgroundImageViewConstraints;
+@property (nonatomic, strong) UIImageView *productImageView;
+
+@end
+
 @implementation BUYProductViewHeader
 
 - (instancetype)initWithFrame:(CGRect)frame
@@ -15,12 +23,31 @@
 	self = [super initWithFrame:frame];
 	if (self) {
 		self.backgroundColor = [UIColor redColor];
+		
+		self.backgroundImageView = [[UIImageView alloc] init];
+		self.backgroundImageView.translatesAutoresizingMaskIntoConstraints = NO;
+		self.backgroundImageView.backgroundColor = [UIColor blueColor];
+		self.backgroundImageView.contentMode = UIViewContentModeScaleAspectFill;
+		[self addSubview:self.backgroundImageView];
+		
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_backgroundImageView]|"
+																	 options:0
+																	 metrics:nil
+																	   views:NSDictionaryOfVariableBindings(_backgroundImageView)]];
 	}
 	return self;
 }
 
 - (void)setContentOffset:(CGPoint)offset
 {
+	NSLog(@"%@", NSStringFromCGPoint(offset));
+	
+	[self removeConstraints:self.backgroundImageViewConstraints];
+	self.backgroundImageViewConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:[_backgroundImageView(backgroundImageViewHeight)]|"
+																				  options:0
+																				  metrics:@{ @"backgroundImageViewHeight" : @(CGRectGetHeight(self.bounds) + -offset.y) }
+																					views:NSDictionaryOfVariableBindings(_backgroundImageView)];
+	[self addConstraints:self.backgroundImageViewConstraints];
 	
 }
 
