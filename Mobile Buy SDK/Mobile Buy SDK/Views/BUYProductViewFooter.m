0fdc71536b0db4c17f598c432252899a47103498
Rune Madsen <runemadsen@mac.com>
2015-07-08 10:13:19 -0400

    tintColorDidChange

--- Mobile Buy SDK/Mobile Buy SDK/Views/BUYProductViewFooter.m
+++ Mobile Buy SDK/Mobile Buy SDK/Views/BUYProductViewFooter.m
@@ -98,6 +98,12 @@
 	}
 }
 
+- (void)tintColorDidChange
+{
+	[super tintColorDidChange];
+	self.checkoutButton.backgroundColor = self.tintColor;
+}
+
 /*
  // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
