f97809a792b1d2821022e2c1d68e32f25933a2d6
David Muzi <david.muzi@shopify.com>
2015-07-09 10:23:00 -0400

    custom presentation animation

--- Mobile Buy SDK/Mobile Buy SDK/View Controllers/BUYNavigationController.m
+++ Mobile Buy SDK/Mobile Buy SDK/View Controllers/BUYNavigationController.m
@@ -9,7 +9,7 @@
 #import "BUYNavigationController.h"
 #import "BUYPresentationController.h"
 
-@interface BUYNavigationController () <UIViewControllerTransitioningDelegate>
+@interface BUYNavigationController () <UIViewControllerTransitioningDelegate, UIViewControllerAnimatedTransitioning>
 
 @end
 
@@ -32,10 +32,62 @@
 	return self;
 }
 
+#pragma mark - Transitioning Delegate Methods
+
 - (UIPresentationController *)presentationControllerForPresentedViewController:(UIViewController *)presented presentingViewController:(UIViewController *)presenting sourceViewController:(UIViewController *)source
 {
 	UIPresentationController *presentationController = [[BUYPresentationController alloc] initWithPresentedViewController:presented presentingViewController:presenting];
 	return presentationController;
+}
+
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source
+{
+	return self;
+}
+
+- (id <UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed
+{
+	return self;
+}
+
+#pragma mark - Animated Transitioning Methods
+
+- (NSTimeInterval)transitionDuration:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+	return 0.3;
+}
+
+- (void)animateTransition:(id<UIViewControllerContextTransitioning>)transitionContext
+{
+	UIViewController *presentedController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
+
+	if (presentedController == self) {
+		
+		UIView *presentedView = presentedController.view;
+		CGRect finalRect = [transitionContext finalFrameForViewController:presentedController];
+		presentedView.frame = finalRect;
+		presentedView.alpha = 0.0;
+
+		[[transitionContext containerView] addSubview:presentedView];
+		
+		[UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+			presentedView.alpha = 1.0;
+		} completion:^(BOOL finished) {
+			[transitionContext completeTransition:finished];
+		}];
+	}
+	else {
+		
+		UIView *dismissedView = [transitionContext viewForKey:UITransitionContextFromViewKey];
+		
+		[UIView animateWithDuration:[self transitionDuration:transitionContext] animations:^{
+			dismissedView.alpha = 0.0;
+		} completion:^(BOOL finished) {
+			[dismissedView removeFromSuperview];
+			[transitionContext completeTransition:finished];
+		}];
+	}
 }
 
 @end
