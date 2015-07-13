7f432109837a620106fac28e68cabaef3eae6891
Rune Madsen <runemadsen@mac.com>
2015-07-12 22:57:48 -0400

    remove hairline

--- Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductViewController.m
+++ Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductViewController.m
@@ -208,12 +208,6 @@
 														   constant:64]];
 }
 
-- (void)viewDidLoad
-{
-	[super viewDidLoad];
-	self.navigationBarShadowImageView = [self findHairlineImageViewUnder:self.navigationController.navigationBar];
-}
-
 - (void)viewDidLayoutSubviews
 {
 	[super viewDidLayoutSubviews];
@@ -358,19 +352,6 @@
 	if ([self.productDelegate respondsToSelector:@selector(productViewControllerDidFinish:)]) {
 		[self.productDelegate productViewControllerDidFinish:self];
 	}
-}
-
-- (UIImageView *)findHairlineImageViewUnder:(UIView *)view {
-	if ([view isKindOfClass:UIImageView.class] && view.bounds.size.height <= 1.0) {
-		return (UIImageView *)view;
-	}
-	for (UIView *subview in view.subviews) {
-		UIImageView *imageView = [self findHairlineImageViewUnder:subview];
-		if (imageView) {
-			return imageView;
-		}
-	}
-	return nil;
 }
 
 @end
