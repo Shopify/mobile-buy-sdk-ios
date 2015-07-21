a0c33d208ee9bc5f9931eef5048c661844ba19ec
David Muzi <david.muzi@shopify.com>
2015-07-21 14:45:11 -0400

    Move test client outside main umbrella header

--- Mobile Buy SDK/Mobile Buy SDK/Buy.h
+++ Mobile Buy SDK/Mobile Buy SDK/Buy.h
@@ -14,7 +14,6 @@
 #import <Buy/BUYGiftCard.h>
 #import <Buy/BUYLineItem.h>
 #import <Buy/BUYClient.h>
-#import <Buy/BUYClient+Test.h>
 #import <Buy/BUYImage.h>
 #import <Buy/BUYOption.h>
 #import <Buy/BUYOptionValue.h>
--- Mobile Buy SDK/Mobile Buy SDK/Data/BUYClient+Test.h
+++ Mobile Buy SDK/Mobile Buy SDK/Data/BUYClient+Test.h
@@ -10,7 +10,7 @@
 @interface BUYClient (Test)
 
 /**
- *  Test the integration with your shop.  This should not be shipped in production code
+ *  Test the integration with your shop.  This should not be shipped in production code.  This is a syncronous network call.
  *  @param merchantId the merchant ID setup for Apple Pay in the Integration page of the Mobile Channel
  *
  *  @return YES on success
