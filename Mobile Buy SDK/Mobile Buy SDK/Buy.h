6255856a8caf75729d93c3ef56e419038f630009
Rune Madsen <runemadsen@mac.com>
2015-07-22 09:38:44 -0400

    Add BUYClient+Test to Buy header

--- Mobile Buy SDK/Mobile Buy SDK/Buy.h
+++ Mobile Buy SDK/Mobile Buy SDK/Buy.h
@@ -6,27 +6,28 @@
 //  Copyright (c) 2015 Shopify Inc. All rights reserved.
 //
 
+#import <Buy/Buy-Bridging-Header.h>
 #import <Buy/BUYAddress.h>
+#import <Buy/BUYApplePayAdditions.h>
+#import <Buy/BUYApplePayHelpers.h>
 #import <Buy/BUYCart.h>
 #import <Buy/BUYCheckout.h>
+#import <Buy/BUYClient.h>
+#import <Buy/BUYClient+Test.h>
 #import <Buy/BUYCreditCard.h>
 #import <Buy/BUYDiscount.h>
 #import <Buy/BUYGiftCard.h>
-#import <Buy/BUYLineItem.h>
-#import <Buy/BUYClient.h>
 #import <Buy/BUYImage.h>
+#import <Buy/BUYLineItem.h>
 #import <Buy/BUYOption.h>
 #import <Buy/BUYOptionValue.h>
+#import <Buy/BUYPaymentButton.h>
 #import <Buy/BUYProduct.h>
 #import <Buy/BUYProductVariant.h>
+#import <Buy/BUYProductViewController.h>
 #import <Buy/BUYShippingRate.h>
 #import <Buy/BUYShop.h>
 #import <Buy/BUYStoreViewController.h>
 #import <Buy/BUYTaxLine.h>
-#import <Buy/BUYViewController.h>
-#import <Buy/BUYApplePayAdditions.h>
-#import <Buy/BUYApplePayHelpers.h>
-#import <Buy/BUYPaymentButton.h>
-#import <Buy/BUYProductViewController.h>
 #import <Buy/BUYTheme.h>
-#import <Buy/Buy-Bridging-Header.h>
+#import <Buy/BUYViewController.h>
