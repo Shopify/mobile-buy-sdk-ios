9afe2336627d4e4cf52679bc7cb9b7e565f35809
Rune Madsen <runemadsen@mac.com>
2015-08-05 16:55:04 -0400

    container view for blur effect + no WHITE for david

--- Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductViewHeaderOverlay.m
+++ Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductViewHeaderOverlay.m
@@ -11,8 +11,8 @@
 
 @interface BUYProductViewHeaderOverlay ()
 
-@property (nonatomic, strong) UIView *whiteOverlayView;
-@property (nonatomic, strong) UIVisualEffectView *visualEffectView;
+@property (nonatomic, strong) UIView *overlayView;
+@property (nonatomic, strong) UIView *visualEffectContainerView;
 
 @end
 
@@ -25,42 +25,48 @@
 		self.userInteractionEnabled = NO;
 		self.backgroundColor = [UIColor clearColor];
 		
+		_visualEffectContainerView = [[UIView alloc] init];
+		_visualEffectContainerView.translatesAutoresizingMaskIntoConstraints = NO;
+		_visualEffectContainerView.alpha = 0;
+		[self addSubview:_visualEffectContainerView];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_visualEffectContainerView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_visualEffectContainerView)]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_visualEffectContainerView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_visualEffectContainerView)]];
+		
 		UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:(theme.style == BUYThemeStyleDark ? UIBlurEffectStyleDark : UIBlurEffectStyleLight)];
-		_visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-		_visualEffectView.alpha = 0;
-		_visualEffectView.translatesAutoresizingMaskIntoConstraints = NO;
-		[self addSubview:_visualEffectView];
+		UIVisualEffectView *visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+		visualEffectView.translatesAutoresizingMaskIntoConstraints = NO;
+		[_visualEffectContainerView addSubview:visualEffectView];
 		
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_visualEffectView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_visualEffectView)]];
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_visualEffectView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_visualEffectView)]];
+		[_visualEffectContainerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[visualEffectView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(visualEffectView)]];
+		[_visualEffectContainerView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[visualEffectView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(visualEffectView)]];
 		
-		_whiteOverlayView = [[UIView alloc] init];
-		_whiteOverlayView.alpha = 0;
-		_whiteOverlayView.backgroundColor = (theme.style == BUYThemeStyleDark) ? BUY_RGB(26, 26, 26) : BUY_RGB(255, 255, 255);
-		_whiteOverlayView.translatesAutoresizingMaskIntoConstraints = NO;
-		[self addSubview:_whiteOverlayView];
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_whiteOverlayView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_whiteOverlayView)]];
-		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_whiteOverlayView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_whiteOverlayView)]];
+		_overlayView = [[UIView alloc] init];
+		_overlayView.alpha = 0;
+		_overlayView.backgroundColor = (theme.style == BUYThemeStyleDark) ? BUY_RGB(26, 26, 26) : BUY_RGB(255, 255, 255);
+		_overlayView.translatesAutoresizingMaskIntoConstraints = NO;
+		[self addSubview:_overlayView];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_overlayView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_overlayView)]];
+		[self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_overlayView]|" options:0 metrics:nil views:NSDictionaryOfVariableBindings(_overlayView)]];
 	}
 	return self;
 }
 
 static CGFloat visualEffectViewThreshold = 200.0f;
-static CGFloat whiteOverlayViewThreshold = 100.0f;
+static CGFloat overlayViewThreshold = 100.0f;
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView withNavigationBarHeight:(CGFloat)navigationBarHeight
 {
 	if (scrollView.contentOffset.y > 0) {
 		CGFloat opaqueOffset = CGRectGetHeight(self.bounds) - navigationBarHeight;
 		
-		CGFloat whiteStartingOffset = opaqueOffset - whiteOverlayViewThreshold;
-		_whiteOverlayView.alpha = (scrollView.contentOffset.y - whiteStartingOffset) / (opaqueOffset - whiteStartingOffset);
+		CGFloat overlayStartingOffset = opaqueOffset - overlayViewThreshold;
+		self.overlayView.alpha = (scrollView.contentOffset.y - overlayStartingOffset) / (opaqueOffset - overlayStartingOffset);
 		
 		CGFloat blurStartingOffset = opaqueOffset - visualEffectViewThreshold;
-		_visualEffectView.alpha = (scrollView.contentOffset.y - blurStartingOffset) / (opaqueOffset - blurStartingOffset);
+		self.visualEffectContainerView.alpha = (scrollView.contentOffset.y - blurStartingOffset) / (opaqueOffset - blurStartingOffset);
 	} else {
-		self.whiteOverlayView.alpha = 0;
-		self.visualEffectView.alpha = 0;
+		self.overlayView.alpha = 0;
+		self.visualEffectContainerView.alpha = 0;
 	}
 }
 
