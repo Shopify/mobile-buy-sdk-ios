86f7b83b783d317be7ba19fa5f98faabb0b65290
Rune Madsen <runemadsen@mac.com>
2015-08-05 16:31:46 -0400

    Fixed image changes. Adding custom disclosure indicator for variant selector Related: #326

--- Mobile Buy SDK/Mobile Buy SDK/Product View/Variant Selection/BUYOptionValueCell.m
+++ Mobile Buy SDK/Mobile Buy SDK/Product View/Variant Selection/BUYOptionValueCell.m
@@ -11,7 +11,9 @@
 #import "BUYOptionValue.h"
 
 @interface BUYOptionValueCell()
-@property (nonatomic, strong) NSLayoutConstraint *imageConstraint;
+@property (nonatomic, strong) NSArray *disclosureConstraint;
+@property (nonatomic, strong) NSArray *noDisclosureConstraint;
+@property (nonatomic, strong) UIImageView *disclosureIndicatorImageView;
 @end
 
 @implementation BUYOptionValueCell
@@ -40,26 +42,32 @@
 		[_selectedImageView setContentCompressionResistancePriority:UILayoutPriorityRequired forAxis:UILayoutConstraintAxisHorizontal];
 		[self.contentView addSubview:_selectedImageView];
 		
+		_disclosureIndicatorImageView = [[UIImageView alloc] init];
+		_disclosureIndicatorImageView.translatesAutoresizingMaskIntoConstraints = NO;
+		[self.contentView addSubview:_disclosureIndicatorImageView];
 		
-		NSDictionary *views = NSDictionaryOfVariableBindings(_titleLabel, _selectedImageView);
+		NSDictionary *views = NSDictionaryOfVariableBindings(_titleLabel, _selectedImageView, _disclosureIndicatorImageView);
 		
-		[self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_titleLabel]-[_selectedImageView]" options:0 metrics:nil views:views]];
+		self.disclosureConstraint = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_titleLabel]-[_selectedImageView]-[_disclosureIndicatorImageView]-(12)-|" options:0 metrics:nil views:views];
+		self.noDisclosureConstraint = [NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_titleLabel]-[_selectedImageView]-(12)-|" options:0 metrics:nil views:views];
+		
 		[self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_titleLabel]|" options:0 metrics:nil views:views]];
-
+		
 		[self.contentView addConstraint: [NSLayoutConstraint constraintWithItem:_selectedImageView
 																	  attribute:NSLayoutAttributeCenterY
 																	  relatedBy:NSLayoutRelationEqual
 																		 toItem:_selectedImageView.superview
 																	  attribute:NSLayoutAttributeCenterY
-																	 multiplier:1.f constant:0.f]];
+																	 multiplier:1.0f
+																	   constant:0.0f]];
 		
-		_imageConstraint = [NSLayoutConstraint constraintWithItem:_selectedImageView
-														attribute:NSLayoutAttributeTrailing
-														relatedBy:NSLayoutRelationEqual
-														   toItem:_selectedImageView.superview
-														attribute:NSLayoutAttributeTrailing
-													   multiplier:1.f constant:0.f];
-		[self.contentView addConstraint:_imageConstraint];
+		[self.contentView addConstraint: [NSLayoutConstraint constraintWithItem:_disclosureIndicatorImageView
+																	  attribute:NSLayoutAttributeCenterY
+																	  relatedBy:NSLayoutRelationEqual
+																		 toItem:_disclosureIndicatorImageView.superview
+																	  attribute:NSLayoutAttributeCenterY
+																	 multiplier:1.0f
+																	   constant:0.0f]];
 		
 	}
 	return self;
@@ -67,8 +75,14 @@
 
 - (void)setAccessoryType:(UITableViewCellAccessoryType)accessoryType
 {
-	[super setAccessoryType:accessoryType];
-	self.imageConstraint.constant = accessoryType == UITableViewCellAccessoryNone ? -12.0f : 0.0f;
+	self.disclosureIndicatorImageView.hidden = accessoryType != UITableViewCellAccessoryDisclosureIndicator;
+	if (accessoryType == UITableViewCellAccessoryDisclosureIndicator) {
+		[NSLayoutConstraint activateConstraints:self.disclosureConstraint];
+		[NSLayoutConstraint deactivateConstraints:self.noDisclosureConstraint];
+	} else {
+		[NSLayoutConstraint deactivateConstraints:self.disclosureConstraint];
+		[NSLayoutConstraint activateConstraints:self.noDisclosureConstraint];
+	}
 }
 
 - (void)setOptionValue:(BUYOptionValue *)optionValue
@@ -84,6 +98,18 @@
 	self.selectedImageView.tintColor = theme.tintColor;
 	self.backgroundColor = theme.style == BUYThemeStyleDark ? BUY_RGB(26, 26, 26) : BUY_RGB(255, 255, 255);
 	self.selectedBackgroundView.backgroundColor = theme.style == BUYThemeStyleDark ? BUY_RGB(60, 60, 60) : BUY_RGB(242, 242, 242);
+	UIImage *disclosureIndicatorImage;
+	switch (theme.style) {
+		case BUYThemeStyleDark:
+			disclosureIndicatorImage = [BUYImageKit imageOfDisclosureIndicatorImageWithFrame:CGRectMake(0, 0, 10, 16) color:BUY_RGB(76, 76, 76)];
+			break;
+		case BUYThemeStyleLight:
+			disclosureIndicatorImage = [BUYImageKit imageOfDisclosureIndicatorImageWithFrame:CGRectMake(0, 0, 10, 16) color:BUY_RGB(191, 191, 191)];
+			break;
+		default:
+			break;
+	}
+	_disclosureIndicatorImageView.image = disclosureIndicatorImage;
 }
 
 - (void)prepareForReuse
