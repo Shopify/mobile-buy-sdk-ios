f51a26cc535fcf2e87b1674d010304b45f24383c
Rune Madsen <runemadsen@mac.com>
2015-08-05 10:43:55 -0400

    Fix #361

--- Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductViewFooter.m
+++ Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductViewFooter.m
@@ -115,12 +115,4 @@
 	self.checkoutButton.backgroundColor = self.tintColor;
 }
 
-/*
- // Only override drawRect: if you perform custom drawing.
- // An empty implementation adversely affects performance during animation.
- - (void)drawRect:(CGRect)rect {
- // Drawing code
- }
- */
-
 @end
