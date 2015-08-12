1419fa8b39db68a65a4ef515ff6bb493b1716973
David Muzi <david.muzi@shopify.com>
2015-08-12 11:02:42 -0400

    More BUYClient unit tests

--- Mobile Buy SDK/Mobile Buy SDK/View Controllers/BUYViewController.m
+++ Mobile Buy SDK/Mobile Buy SDK/View Controllers/BUYViewController.m
@@ -262,7 +262,11 @@
 	}];
 }
 
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
 - (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller didSelectShippingMethod:(nonnull PKShippingMethod *)shippingMethod completion:(nonnull void (^)(PKPaymentAuthorizationStatus, NSArray<PKPaymentSummaryItem *> * _Nonnull))completion
+#else
+- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller didSelectShippingMethod:(PKShippingMethod *)shippingMethod completion:(void (^)(PKPaymentAuthorizationStatus status, NSArray *summaryItems))completion
+#endif
 {
 	[self.applePayHelper updateCheckoutWithShippingMethod:shippingMethod completion:^(PKPaymentAuthorizationStatus status, NSArray *methods) {
 		
@@ -276,7 +280,11 @@
 	}];
 }
 
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
 - (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller didSelectShippingAddress:(ABRecordRef)address completion:(void (^)(PKPaymentAuthorizationStatus, NSArray<PKShippingMethod *> * _Nonnull, NSArray<PKPaymentSummaryItem *> * _Nonnull))completion
+#else
+- (void)paymentAuthorizationViewController:(PKPaymentAuthorizationViewController *)controller didSelectShippingAddress:(ABRecordRef)address completion:(void (^)(PKPaymentAuthorizationStatus status, NSArray *shippingMethods, NSArray *summaryItems))completion
+#endif
 {
 	[self.applePayHelper updateCheckoutWithAddress:address completion:^(PKPaymentAuthorizationStatus status, NSArray *shippingMethods, NSArray *summaryItems) {
 		
