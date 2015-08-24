1a7c57c46268a8c890212f022e022c1d1f99f591
Rune Madsen <runemadsen@mac.com>
2015-08-24 10:02:03 -0400

    add guard

--- Mobile Buy SDK/Mobile Buy SDK/View Controllers/BUYViewController.m
+++ Mobile Buy SDK/Mobile Buy SDK/View Controllers/BUYViewController.m
@@ -42,11 +42,15 @@
 - (void)setClient:(BUYClient *)client
 {
 	_client = client;
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
 	if (&PKPaymentNetworkDiscover != NULL) {
 		self.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa, PKPaymentNetworkDiscover];
 	} else {
+#endif
 		self.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa];
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= 90000
 	}
+#endif
 }
 
 - (BUYClient*)client
