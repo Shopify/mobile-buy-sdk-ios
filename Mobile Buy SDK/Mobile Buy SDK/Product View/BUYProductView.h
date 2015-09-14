6c76756f578c8d680ac73d5ef78303c6a6f31a45
Rune Madsen <runemadsen@mac.com>
2015-09-14 17:07:08 -0400

    more documentation

--- Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductView.h
+++ Mobile Buy SDK/Mobile Buy SDK/Product View/BUYProductView.h
@@ -33,14 +33,43 @@
 @class BUYImage;
 @class BUYProduct;
 
+/**
+ *  The BUYProductViewController's main view, containing everything needed to display the UI for the product
+ */
 @interface BUYProductView : UIView
 
+/**
+ *  The table view containg the product's image(s) in the tableHeaderView, title, price, 
+ *  description and variant options
+ */
 @property (nonatomic, strong) UITableView *tableView;
+
+/**
+ *  The product view includes a blurred product image as the background. The table view's background 
+ *  is clear for this reason. This view ensures the bottom part of the table view is always opaque and only
+ *  the background of the product image(s) shows a blurred product image.
+ */
 @property (nonatomic, strong) UIView *stickyFooterView;
+
+/**
+ *  A constraint that managed the height for the stickyFooterView
+ */
 @property (nonatomic, strong) NSLayoutConstraint *footerHeightLayoutConstraint;
+
+/**
+ *  A constraint that managed the offset for the stickyFooterView based on the current scroll offset
+ */
 @property (nonatomic, strong) NSLayoutConstraint *footerOffsetLayoutConstraint;
+
+/**
+ *  The tableHeaderView containting the product image(s) (if available)
+ */
 @property (nonatomic, strong) BUYProductViewHeader *productViewHeader;
 @property (nonatomic, strong) BUYProductViewHeaderBackgroundImageView *backgroundImageView;
+
+/**
+ *  The footer view containing the Checkout button, and - if enabled - Apple Pay button.
+ */
 @property (nonatomic, strong) BUYProductViewFooter *productViewFooter;
 @property (nonatomic, strong) BUYGradientView *topGradientView;
 @property (nonatomic, weak) BUYTheme *theme;
