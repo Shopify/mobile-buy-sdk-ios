bff37c4d21fa207a281b9d0eeaa5847ec2597664
Rune Madsen <runemadsen@mac.com>
2015-10-20 13:33:23 -0400

    Fixing location of umbrella headers

--- Mobile Buy SDK/Mobile Buy SDK/Static Framework/Buy.h
+++ Mobile Buy SDK/Mobile Buy SDK/Static Framework/Buy.h
@@ -24,40 +24,36 @@
 //  THE SOFTWARE.
 //
 
-#import <UIKit/UIKit.h>
+/**
+ *  Umbrella header used for Cocoapods
+ */
 
-//! Project version number for Buy.
-FOUNDATION_EXPORT double BuyVersionNumber;
-
-//! Project version string for Buy.
-FOUNDATION_EXPORT const unsigned char BuyVersionString[];
-
-#import <Buy/BUYAddress.h>
-#import <Buy/BUYCart.h>
-#import <Buy/BUYCheckout.h>
-#import <Buy/BUYCreditCard.h>
-#import <Buy/BUYDiscount.h>
-#import <Buy/BUYGiftCard.h>
-#import <Buy/BUYLineItem.h>
-#import <Buy/BUYClient.h>
-#import <Buy/BUYClient+Test.h>
-#import <Buy/BUYImage.h>
-#import <Buy/BUYOption.h>
-#import <Buy/BUYOptionValue.h>
-#import <Buy/BUYOrder.h>
-#import <Buy/BUYProduct.h>
-#import <Buy/BUYProductVariant.h>
-#import <Buy/BUYShippingRate.h>
-#import <Buy/BUYShop.h>
-#import <Buy/BUYStoreViewController.h>
-#import <Buy/BUYTaxLine.h>
-#import <Buy/BUYViewController.h>
-#import <Buy/BUYApplePayAdditions.h>
-#import <Buy/BUYApplePayHelpers.h>
-#import <Buy/BUYPaymentButton.h>
-#import <Buy/BUYProductViewController.h>
-#import <Buy/BUYTheme.h>
-#import <Buy/BUYCartLineItem.h>
-#import <Buy/BUYCollection.h>
-#import <Buy/BUYMaskedCreditCard.h>
-#import <Buy/BUYError.h>
+#import "BUYApplePayAdditions.h"
+#import "BUYApplePayHelpers.h"
+#import "BUYAddress.h"
+#import "BUYCart.h"
+#import "BUYCartLineItem.h"
+#import "BUYCheckout.h"
+#import "BUYClient+Test.h"
+#import "BUYClient.h"
+#import "BUYCollection.h"
+#import "BUYCreditCard.h"
+#import "BUYDiscount.h"
+#import "BUYError.h"
+#import "BUYGiftCard.h"
+#import "BUYImage.h"
+#import "BUYLineItem.h"
+#import "BUYMaskedCreditCard.h"
+#import "BUYOption.h"
+#import "BUYOptionValue.h"
+#import "BUYOrder.h"
+#import "BUYPaymentButton.h"
+#import "BUYProduct.h"
+#import "BUYProductVariant.h"
+#import "BUYProductViewController.h"
+#import "BUYShippingRate.h"
+#import "BUYShop.h"
+#import "BUYStoreViewController.h"
+#import "BUYTaxLine.h"
+#import "BUYTheme.h"
+#import "BUYViewController.h"
