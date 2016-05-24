0935d49be490435e1ddd1dcd33ccf6176b718f5f
Dima Bart <dima.bart01@gmail.com>
2016-05-24 09:41:18 -0400

    Improve test coverage for BUYOperation subclasses.

--- Mobile Buy SDK/Mobile Buy SDK Tests/BUYRequestOperationTests.m
+++ Mobile Buy SDK/Mobile Buy SDK Tests/BUYRequestOperationTests.m
@@ -2,8 +2,26 @@
 //  BUYRequestOperationTests.m
 //  Mobile Buy SDK
 //
-//  Created by Dima Bart on 2016-05-16.
-//  Copyright © 2016 Shopify Inc. All rights reserved.
+//  Created by Shopify.
+//  Copyright (c) 2015 Shopify Inc. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import <XCTest/XCTest.h>
@@ -239,8 +257,8 @@
 	
 	[self createExpectationDelay];
 	
-	[operation cancel];
 	[self.queue addOperation:operation];
+	[operation cancel];
 	
 	[self waitForExpectationsWithTimeout:3.0 handler:nil];
 }
@@ -259,6 +277,21 @@
 		[operation cancel];
 	}];
 	
+	[self waitForExpectationsWithTimeout:4.0 handler:nil];
+}
+
+- (void)testCancellationWithoutQueue
+{
+	[self stubRequestsWithDelay:0.5];
+	
+	BUYRequestOperation *operation = [self operationFulfillingExpectation:nil completion:^{
+		XCTAssert(NO, @"Operation should not call completion if cancelled.");
+	}];
+	
+	[operation start];
+	[operation cancel];
+	
+	[self createExpectationDelay];
 	[self waitForExpectationsWithTimeout:4.0 handler:nil];
 }
 
