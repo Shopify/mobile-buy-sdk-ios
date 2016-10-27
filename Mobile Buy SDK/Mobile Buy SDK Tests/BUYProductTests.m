79f04a834b6761cb6b9799a6bee5590089103e21
Eishan Vijay <eishan.vijay@shopify.com>
2016-10-27 17:00:24 -0400

    Add tests for updated default variant checker

--- Mobile Buy SDK/Mobile Buy SDK Tests/BUYProductTests.m
+++ Mobile Buy SDK/Mobile Buy SDK Tests/BUYProductTests.m
@@ -2,19 +2,74 @@
 //  BUYProductTests.m
 //  Mobile Buy SDK
 //
-//  Created by Brent Gulanowski on 2016-10-31.
-//  Copyright © 2016 Shopify Inc. All rights reserved.
+//  Created by Shopify.
+//  Copyright (c) 2016 Shopify Inc. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
-#import <XCTest/XCTest.h>
-
+@import XCTest;
 @import Buy;
 
 @interface BUYProductTests : XCTestCase
-
 @end
 
 @implementation BUYProductTests
+
+# pragma mark - Default Variant Checker Tests
+
+- (void)testDefaultVariantCheckerWithCorrectOptionValues {
+	BUYModelManager *model = [BUYModelManager modelManager];
+	BUYProduct *product = [[BUYProduct alloc] init];
+	BUYProductVariant *variant = [[BUYProductVariant alloc] initWithModelManager:model
+																  JSONDictionary:@{ @"id" : @1}];
+	
+	BUYOptionValue *option = [model buy_objectWithEntityName:[BUYOptionValue entityName] JSONDictionary:@{
+																										  @"name"      : @"Title",
+																										  @"value"     : @"Default",
+																										  @"option_id" : @1,
+																										  }];
+	variant.options = [[NSSet alloc] initWithArray:@[option]];
+	product.variants = [[NSOrderedSet alloc] initWithArray:@[variant]];
+	XCTAssertTrue([product isDefaultVariant]);
+}
+
+- (void)testDefaultVariantCheckerWithIncorrectOptionValues {
+	BUYModelManager *model = [BUYModelManager modelManager];
+	BUYProduct *product = [[BUYProduct alloc] init];
+	BUYProductVariant *variant = [[BUYProductVariant alloc] initWithModelManager:model
+																  JSONDictionary:@{ @"id" : @1}];
+	
+	BUYOptionValue *option = [model buy_objectWithEntityName:[BUYOptionValue entityName] JSONDictionary:@{
+																										  @"name"      : @"name1",
+																										  @"value"     : @"value1",
+																										  @"option_id" : @1,
+																										}];
+	variant.options = [[NSSet alloc] initWithArray:@[option]];
+	product.variants = [[NSOrderedSet alloc] initWithArray:@[variant]];
+	XCTAssertFalse([product isDefaultVariant]);
+}
+
+- (void)testDefaultVariantCheckerWithEmptyProduct {
+	BUYProduct *product = [[BUYProduct alloc] init];
+	XCTAssertFalse([product isDefaultVariant]);
+}
 
 - (void)testMinimumPrices
 {
