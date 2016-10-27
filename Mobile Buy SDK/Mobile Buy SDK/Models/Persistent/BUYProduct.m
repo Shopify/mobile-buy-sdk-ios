0ebcfcafd0eb736776d2da28749c73cda3b189c2
Eishan Vijay <eishan.vijay@shopify.com>
2016-10-27 09:18:53 -0400

    Update default variant check

--- Mobile Buy SDK/Mobile Buy SDK/Models/Persistent/BUYProduct.m
+++ Mobile Buy SDK/Mobile Buy SDK/Models/Persistent/BUYProduct.m
@@ -148,12 +148,15 @@
 {
 	if ([self.variants count] == 1) {
 		BUYProductVariant *productVariant = [self.variants firstObject];
-		BUYOptionValue *optionValue = [productVariant.options anyObject];
-		NSString *defaultTitleString = @"Default Title";
-		NSString *defaultString = @"Default";
-		if ([productVariant.title isEqualToString:defaultTitleString] &&
-			([optionValue.value isEqualToString:defaultTitleString] || [optionValue.value isEqualToString:defaultString])) {
-			return YES;
+		if ([productVariant.options count] == 1) {
+			BUYOptionValue *optionValue = [productVariant.options anyObject];
+			NSString *defaultTitleString = @"Default Title";
+			NSString *defaultString = @"Default";
+			NSString *titleString = @"Title";
+			if ([optionValue.name isEqualToString:titleString] &&
+				([optionValue.value isEqualToString:defaultTitleString] || [optionValue.value isEqualToString:defaultString])) {
+				return YES;
+			}
 		}
 	}
 	return NO;
