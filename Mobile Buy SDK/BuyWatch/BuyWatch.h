6ec0610e1d561dc8748a5ad84509caac56436549
Eishan Vijay <eishan.vijay@shopify.com>
2016-12-02 12:15:44 -0500

    Add new target

---
+++ Mobile Buy SDK/BuyWatch/BuyWatch.h
@@ -0,0 +1,19 @@
+//
+//  BuyWatch.h
+//  BuyWatch
+//
+//  Created by Eishan Vijay on 2016-12-02.
+//  Copyright © 2016 Shopify Inc. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BuyWatch.
+FOUNDATION_EXPORT double BuyWatchVersionNumber;
+
+//! Project version string for BuyWatch.
+FOUNDATION_EXPORT const unsigned char BuyWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BuyWatch/PublicHeader.h>
+
+
