e3cd35103cec88e96fb4de07a9f4f86ee5719acc
David Muzi <david.muzi@shopify.com>
2017-01-17 10:58:50 -0500

    update unit tests to run on iOS 10.2

--- Scripts/run_tests.sh
+++ Scripts/run_tests.sh
@@ -14,4 +14,4 @@ fi
 xcodebuild test -project "Mobile Buy SDK/Mobile Buy SDK.xcodeproj" \
 -scheme "Mobile Buy SDK Tests" \
 -sdk iphonesimulator \
--destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.1'
+-destination 'platform=iOS Simulator,name=iPhone 6s,OS=10.2'
