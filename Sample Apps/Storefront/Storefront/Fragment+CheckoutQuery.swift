2c888588c2f2323f606448f6ffd81a2770ce70c4
Dima Bart <dima.bart01@gmail.com>
2017-04-04 14:52:42 -0400

    Add fragment for Checkout.

---
+++ Sample Apps/Storefront/Storefront/Fragment+CheckoutQuery.swift
@@ -0,0 +1,79 @@
+//
+//  Fragment+CheckoutQuery.swift
+//  Storefront
+//
+//  Created by Shopify.
+//  Copyright (c) 2017 Shopify Inc. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import Buy
+
+extension Storefront.CheckoutQuery {
+    
+    @discardableResult
+    func fragmentForCheckout() -> Storefront.CheckoutQuery { return self
+        .id()
+        .ready()
+        .requiresShipping()
+        .taxesIncluded()
+        .email()
+        
+        .shippingAddress { $0
+            .firstName()
+            .lastName()
+            .phone()
+            .address1()
+            .address2()
+            .city()
+            .country()
+            .province()
+            .provinceCode()
+            .zip()
+        }
+        
+        .shippingLine { $0
+            .handle()
+            .title()
+            .price()
+        }
+        
+        .note()
+        .lineItems(first: 250) { $0
+            .edges { $0
+                .cursor()
+                .node { $0
+                    .variant { $0
+                        .id()
+                        .price()
+                    }
+                    .title()
+                    .quantity()
+                }
+            }
+        }
+        .webUrl()
+        .currencyCode()
+        .subtotalPrice()
+        .totalTax()
+        .totalPrice()
+        .paymentDue()
+    }
+}
