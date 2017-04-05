d4b4201f9562b4db08daf59c6731508f85168f65
Dima Bart <dima.bart01@gmail.com>
2017-04-04 22:23:17 -0400

    Add queries for checkout mutations.

--- Sample Apps/Storefront/Storefront/ClientQuery.swift
+++ Sample Apps/Storefront/Storefront/ClientQuery.swift
@@ -26,9 +26,13 @@
 
 import Foundation
 import Buy
+import Pay
 
 final class ClientQuery {
 
+    // ----------------------------------
+    //  MARK: - Storefront -
+    //
     static func queryForCollections(limit: Int, after cursor: String? = nil, productLimit: Int = 25, productCursor: String? = nil) -> Storefront.QueryRootQuery {
         return Storefront.buildQuery { $0
             .shop { $0
@@ -69,6 +73,9 @@ final class ClientQuery {
         }
     }
     
+    // ----------------------------------
+    //  MARK: - Checkout -
+    //
     static func mutationForCreateCheckout(with cartItems: [CartItem]) -> Storefront.MutationQuery {
         let lineItems = cartItems.map { item in
             Storefront.LineItemInput(variantId: GraphQL.ID(rawValue: item.variant.id), quantity: Int32(item.quantity))
@@ -79,68 +86,182 @@ final class ClientQuery {
         return Storefront.buildMutation { $0
             .checkoutCreate(input: checkoutInput) { $0
                 .checkout { $0
+                    .fragmentForCheckout()
+                }
+            }
+        }
+    }
+    
+    static func mutationForUpdateCheckout(_ id: String, updatingShippingAddress address: PayPostalAddress) -> Storefront.MutationQuery {
+        
+//        let addressInput = Storefront.MailingAddressInput(
+//            city:     address.city,
+//            country:  address.country,
+//            province: address.province,
+//            zip:      address.zip
+//        )
+        
+        
+        let addressInput = Storefront.MailingAddressInput(
+            address1:  "80 Spadina",
+            address2:  "",
+            city:      "Toronto",
+            country:   "Canada",
+            firstName: "John",
+            lastName:  "Smith",
+            province:  "ON",
+            zip:       "M5V 2J4"
+        )
+        
+        let updateInput = Storefront.CheckoutShippingAddressUpdateInput(shippingAddress: addressInput, checkoutId: GraphQL.ID(rawValue: id))
+        
+        return Storefront.buildMutation { $0
+            .checkoutShippingAddressUpdate(input: updateInput) { $0
+                .userErrors { $0
+                    .field()
+                    .message()
+                }
+                .checkout { $0
+                    .fragmentForCheckout()
+                }
+            }
+        }
+    }
+    
+//    static func mutationForUpdateCheckout(_ id: String, updatingBillingAddress address: PayPostalAddress) -> Storefront.MutationQuery {
+//        
+////        let addressInput = Storefront.MailingAddressInput(
+////            city:     address.city,
+////            country:  address.country,
+////            province: address.province,
+////            zip:      address.zip
+////        )
+//        
+//        let addressInput = Storefront.MailingAddressInput(
+//            address1:  "80 Spadina",
+//            address2:  "",
+//            city:      "Toronto",
+//            country:   "Canada",
+//            firstName: "John",
+//            lastName:  "Smith",
+//            province:  "ON",
+//            zip:       "M5V 2J4"
+//        )
+//        
+//        let updateInput = Storefront.CheckoutBillingAddressUpdateInput(shippingAddress: addressInput, checkoutId: GraphQL.ID(rawValue: id))
+//        
+//        return Storefront.buildMutation { $0
+//            .checkoutBillingAddressUpdate(input: updateInput) { $0
+//                .userErrors { $0
+//                    .field()
+//                    .message()
+//                }
+//                .checkout { $0
+//                    .fragmentForCheckout()
+//                }
+//            }
+//        }
+//    }
+    
+    static func mutationForUpdateCheckout(_ id: String, updatingShippingRate shippingRate: PayShippingRate) -> Storefront.MutationQuery {
+        
+        let updateInput = Storefront.CheckoutShippingLineUpdateInput(checkoutId: GraphQL.ID(rawValue: id), shippingRateHandle: shippingRate.handle)
+        
+        return Storefront.buildMutation { $0
+            .checkoutShippingLineUpdate(input: updateInput) { $0
+                .userErrors { $0
+                    .field()
+                    .message()
+                }
+                .checkout { $0
+                    .fragmentForCheckout()
+                }
+            }
+        }
+    }
+    
+    static func mutationForUpdateCheckout(_ id: String, updatingEmail email: String) -> Storefront.MutationQuery {
+        
+        let updateInput = Storefront.CheckoutEmailUpdateInput(checkoutId: GraphQL.ID(rawValue: id), email: email)
+        
+        return Storefront.buildMutation { $0
+            .checkoutEmailUpdate(input: updateInput) { $0
+                .userErrors { $0
+                    .field()
+                    .message()
+                }
+                .checkout { $0
+                    .fragmentForCheckout()
+                }
+            }
+        }
+    }
+    
+    static func mutationForCompleteCheckoutUsingApplePay(_ checkout: PayCheckout, billingAddress: PayAddress, token: String, idempotencyToken: String) -> Storefront.MutationQuery {
+        
+        let mailingAddress = Storefront.MailingAddressInput(
+            address1:  billingAddress.addressLine1,
+            address2:  billingAddress.addressLine2,
+            city:      billingAddress.city,
+            country:   billingAddress.country,
+            firstName: billingAddress.firstName,
+            lastName:  billingAddress.lastName,
+            province:  billingAddress.province,
+            zip:       billingAddress.zip
+        )
+        
+        let tokenizedInput = Storefront.CheckoutCompleteWithTokenizedPaymentInput(
+            checkoutId:     GraphQL.ID(rawValue: checkout.id),
+            amount:         checkout.paymentDue,
+            idempotencyKey: idempotencyToken,
+            billingAddress: mailingAddress,
+            type:           CheckoutViewModel.PaymentType.applePay.rawValue,
+            paymentData:    token
+        )
+        
+        return Storefront.buildMutation { $0
+            .checkoutCompleteWithTokenizedPayment(input: tokenizedInput) { $0
+                .userErrors { $0
+                    .field()
+                    .message()
+                }
+                .payment { $0
                     .id()
-                    .completedAt()
-                    .createdAt()
-                    .updatedAt()
-                    
                     .ready()
-                    .requiresShipping()
-                    .taxExempt()
-                    .taxesIncluded()
-                    
-                    .shippingAddress { $0
-                        .address1()
-                        .address2()
-                        .city()
-                        .country()
-                        .countryCode()
-                        .province()
-                        .provinceCode()
-                        .zip()
-                        
-                        .company()
+                    .test()
+                    .amount()
+                    .checkout { $0
+                        .fragmentForCheckout()
+                    }
+                    .creditCard { $0
+                        .firstDigits()
+                        .lastDigits()
+                        .maskedNumber()
+                        .brand()
                         .firstName()
                         .lastName()
-                        .name()
-                        .phone()
+                        .expiryMonth()
+                        .expiryYear()
                     }
-                    
-                    .shippingLine { $0
-                        .handle()
-                        .title()
-                        .price()
-                    }
-                    
-                    .customAttributes { $0
-                        .key()
-                        .value()
-                    }
-                    
-                    .note()
-                    .lineItems(first: 250) { $0
-                        .edges { $0
-                            .node { $0
-                                .variant { $0
-                                    .id()
-                                    .price()
-                                }
-                                .title()
-                                .quantity()
-                                .customAttributes { $0
-                                    .key()
-                                    .value()
-                                }
-                            }
+                }
+            }
+        }
+    }
+    
+    static func queryShippingRatesForCheckout(_ id: String) -> Storefront.QueryRootQuery {
+        
+        return Storefront.buildQuery { $0
+            .node(id: GraphQL.ID(rawValue: id)!) { $0
+                .onCheckout { $0
+                    .fragmentForCheckout()
+                    .availableShippingRates { $0
+                        .ready()
+                        .shippingRates { $0
+                            .handle()
+                            .price()
+                            .title()
                         }
                     }
-                    .orderStatusUrl()
-                    .webUrl()
-                    .currencyCode()
-                    .subtotalPrice()
-                    .totalTax()
-                    .totalPrice()
-                    .paymentDue()
                 }
             }
         }
