ede0f2801a6146a78dec5ac719b191a6802945f5
Zhou Li <zhoulijoe@gmail.com>
2017-11-14 16:22:01 +0800

    Don't retry cancelled task

--- Buy/Client/Graph.Task.swift
+++ Buy/Client/Graph.Task.swift
@@ -212,7 +212,7 @@ internal extension Graph {
                 let (model, error) = self.processResponse(data, response, error)
                 
                 DispatchQueue.main.async {
-                    if var retryHandler = retryHandler, retryHandler.canRetry, retryHandler.condition(model, error) == true {
+                    if var retryHandler = retryHandler, !self.isCancelled, retryHandler.canRetry, retryHandler.condition(model, error) == true {
                         
                         /* ---------------------------------
                          ** A retry handler was provided and
