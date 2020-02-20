b75bb20c13fa1ba50a1639826ce0fd201c28315a
Zuwa Omigie <zuwa.omigie@shopify.com>
2020-02-20 15:44:17 -0500

    Add header to Graph.Client.init()

--- Buy/Client/Graph.Client.swift
+++ Buy/Client/Graph.Client.swift
@@ -79,8 +79,9 @@ extension Graph {
         ///     - shopDomain: The domain of your shop (ex: "shopname.myshopify.com").
         ///     - apiKey:     The API key for you app, obtained from the Shopify admin.
         ///     - session:    A `URLSession` to use for this client. If left blank, a session with a `default` configuration will be created.
+        ///     - locale:    The buyer's locale
         ///
-        public init(shopDomain: String, apiKey: String, session: URLSession = URLSession(configuration: URLSessionConfiguration.default)) {
+        public init(shopDomain: String, apiKey: String, session: URLSession = URLSession(configuration: URLSessionConfiguration.default), locale: String = "") {
 
             let shopURL  = Client.urlFor(shopDomain)
             self.apiURL  = Client.urlFor(shopDomain, path: "/api/2019-10/graphql")
@@ -91,6 +92,7 @@ extension Graph {
                 Header.authorization : apiKey,
                 Header.sdkVersion    : Global.frameworkVersion,
                 Header.sdkVariant    : "ios",
+                Header.language      : locale,
             ]
 
             precondition(!apiKey.isEmpty, "API Key is required to the Buy SDK. You can obtain one by adding a Mobile App channel here: \(shopURL.appendingPathComponent("admin"))")
