c1e18420bbe4f955afd96a60e56f69875775a3ea
runner <runner@Mac-1464.local>
2020-04-06 21:04:19 +0000

    Update schema (auto-generated)

--- Buy/Generated/Storefront/Product.swift
+++ Buy/Generated/Storefront/Product.swift
@@ -84,6 +84,16 @@ extension Storefront {
 			return self
 		}
 
+		/// The compare at price of the product across all variants. 
+		@discardableResult
+		open func compareAtPriceRange(alias: String? = nil, _ subfields: (ProductPriceRangeQuery) -> Void) -> ProductQuery {
+			let subquery = ProductPriceRangeQuery()
+			subfields(subquery)
+
+			addField(field: "compareAtPriceRange", aliasSuffix: alias, subfields: subquery)
+			return self
+		}
+
 		/// The date and time when the product was created. 
 		@discardableResult
 		open func createdAt(alias: String? = nil) -> ProductQuery {
@@ -430,6 +440,13 @@ extension Storefront {
 			return self
 		}
 
+		/// The total quantity of inventory in stock for this Product. 
+		@discardableResult
+		open func totalInventory(alias: String? = nil) -> ProductQuery {
+			addField(field: "totalInventory", aliasSuffix: alias)
+			return self
+		}
+
 		/// The date and time when the product was last modified. 
 		@discardableResult
 		open func updatedAt(alias: String? = nil) -> ProductQuery {
@@ -538,6 +555,12 @@ extension Storefront {
 				}
 				return try CollectionConnection(fields: value)
 
+				case "compareAtPriceRange":
+				guard let value = value as? [String: Any] else {
+					throw SchemaViolationError(type: Product.self, field: fieldName, value: fieldValue)
+				}
+				return try ProductPriceRange(fields: value)
+
 				case "createdAt":
 				guard let value = value as? String else {
 					throw SchemaViolationError(type: Product.self, field: fieldName, value: fieldValue)
@@ -642,6 +665,13 @@ extension Storefront {
 				}
 				return value
 
+				case "totalInventory":
+				if value is NSNull { return nil }
+				guard let value = value as? Int else {
+					throw SchemaViolationError(type: Product.self, field: fieldName, value: fieldValue)
+				}
+				return Int32(value)
+
 				case "updatedAt":
 				guard let value = value as? String else {
 					throw SchemaViolationError(type: Product.self, field: fieldName, value: fieldValue)
@@ -692,6 +722,15 @@ extension Storefront {
 
 		func internalGetCollections(alias: String? = nil) -> Storefront.CollectionConnection {
 			return field(field: "collections", aliasSuffix: alias) as! Storefront.CollectionConnection
+		}
+
+		/// The compare at price of the product across all variants. 
+		open var compareAtPriceRange: Storefront.ProductPriceRange {
+			return internalGetCompareAtPriceRange()
+		}
+
+		func internalGetCompareAtPriceRange(alias: String? = nil) -> Storefront.ProductPriceRange {
+			return field(field: "compareAtPriceRange", aliasSuffix: alias) as! Storefront.ProductPriceRange
 		}
 
 		/// The date and time when the product was created. 
@@ -881,6 +920,15 @@ extension Storefront {
 			return field(field: "title", aliasSuffix: alias) as! String
 		}
 
+		/// The total quantity of inventory in stock for this Product. 
+		open var totalInventory: Int32? {
+			return internalGetTotalInventory()
+		}
+
+		func internalGetTotalInventory(alias: String? = nil) -> Int32? {
+			return field(field: "totalInventory", aliasSuffix: alias) as! Int32?
+		}
+
 		/// The date and time when the product was last modified. 
 		open var updatedAt: Date {
 			return internalGetUpdatedAt()
@@ -935,6 +983,10 @@ extension Storefront {
 					case "collections":
 					response.append(internalGetCollections())
 					response.append(contentsOf: internalGetCollections().childResponseObjectMap())
+
+					case "compareAtPriceRange":
+					response.append(internalGetCompareAtPriceRange())
+					response.append(contentsOf: internalGetCompareAtPriceRange().childResponseObjectMap())
 
 					case "images":
 					response.append(internalGetImages())
